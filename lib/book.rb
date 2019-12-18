be2eaae581674f2c77a85f98c38a68da7125b5a6
Mariana Cid <cidmariana5@gmail.com>
2019-12-18 10:40:28 -0700

    Finish iteration 1

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,10 @@
 class Book 
+    attr_reader :title, :author, :publication_year
+
+    def initialize(book_info)
+        @title = book_info[:title]
+        @author = book_info.values_at(:author_first_name, :author_last_name).compact.join(' ')
+        @publication_year = book_info[:publication_date].split(/\W+/)[2]
+    end
+
 end 
